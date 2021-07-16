cb4e90b5ffc1eb8038cf9a58f3e5c42b3c4ededb
Pam <pamela.glownia@gmail.com>
2021-07-16 16:42:25 +0200

    Checking if is required amount of ingredients

--- src/pl/glownia/pamela/MainMenu.java
+++ src/pl/glownia/pamela/MainMenu.java
@@ -13,7 +13,12 @@ public class MainMenu {
         System.out.println("Coffee is ready!");
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("How many cups of coffee you will need: \n>");
+        calculateIngredients(scan);
+        checkIfIsRequiredAmountOfIngredients(scan);
+    }
+
+    static void calculateIngredients(Scanner scan) {
+        System.out.println("How many cups of coffee you will need:");
         int cups = scan.nextInt();
         int water = cups * 200;
         int milk = cups * 50;
@@ -22,5 +27,47 @@ public class MainMenu {
         System.out.println(water + " ml of water");
         System.out.println(milk + " ml of milk");
         System.out.println(coffeeBeans + " g of coffee beans");
+    }
+
+    static void checkIfIsRequiredAmountOfIngredients(Scanner scan) {
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scan.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scan.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeBeans = scan.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scan.nextInt();
+
+        int availableWater = water / 200;
+        int availableMilk = milk / 50;
+        int availableCoffeeBeans = coffeeBeans / 15;
+        int availableCups;
+        if (availableWater <= availableMilk && availableWater <= availableCoffeeBeans) {
+            availableCups = availableWater;
+        } else if (availableMilk <= availableWater && availableMilk <= availableCoffeeBeans) {
+            availableCups = availableMilk;
+        } else {
+            availableCups = availableCoffeeBeans;
+        }
+        if (availableCups > 0) {
+            if (water >= cups * 200) {
+                if (milk >= cups * 50) {
+                    if (coffeeBeans >= cups * 15) {
+                        availableCups = availableCups - cups;
+                        if (availableCups > 0) {
+                            System.out.println("Yes, I can make that amount of coffee (and even " + availableCups + " more than that)");
+                        } else {
+                            System.out.println("Yes, I can make that amount of coffee");
+                        }
+                    }
+                }
+            } else {
+                System.out.println("No, I can make only " + availableCups + " cup(s) of coffee");
+            }
+        } else {
+            System.out.println("No, I can make only 0 cup(s) of coffee");
+
+        }
     }
 }
